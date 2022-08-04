67af0df8954ca0789acea830e593b840a505e172
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-08-05 02:40:48 +0700

    point 4.2

--- src/router/index.js
+++ src/router/index.js
@@ -87,7 +87,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  }
 })
 router.beforeEach(() => {
   NProgress.start()
