fb8b588dc6843e1efe9952874f7517b17d1dc2b0
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-08-04 19:38:50 +0700

    point 1.4

--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -6,12 +6,20 @@
 <script>
 export default {
   props: ['id', 'event'],
+  inject: ['GStore'], // <---- Inject the Global Store
   methods: {
     register() {
       //Assuming sucessful API call to register them
 
       //Set a flash message to appear on the next page loaded which says
       // 'You are successfully registered for ' + this.event.title
+
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.Gstore.flashMessage = ''
+      }, 3000)
 
       this.$router.push({
         name: 'EventDetails',
