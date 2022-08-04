764f7fcdb841391a41332be69095b7db3d88906a
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-08-05 01:05:54 +0700

    point 2.2

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -33,7 +33,7 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from '@vue/runtime-core'
+//import { watchEffect } from '@vue/runtime-core'
 export default {
   name: 'EventListView',
   props: {
@@ -51,17 +51,20 @@ export default {
       totalEvents: 0
     }
   },
-  created() {
-    watchEffect(() => {
-      EventService.getEvents(2, this.page)
-        .then((response) => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count']
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    })
+  //created() {
+  //watchEffect(() => {
+  //eslint-disable-next-line no-unused-vars
+  beforeRouteEnter(routeTo, rouFrom, next) {
+    EventService.getEvents(2, this.page)
+      .then((response) => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch((error) => {
+        this.$router.push({ name: 'NetworkError' })
+        console.log(error)
+      })
+    //})
   },
   computed: {
     hasNextPage() {
