3dcc67c3e602d7b96a30d059f64731db425e1797
Jedsada <Jedsada_kampen@cmu.ac.th>
2022-08-04 19:18:46 +0700

    point 1.3

--- src/views/event/EventRegisterView.vue
+++ src/views/event/EventRegisterView.vue
@@ -1,9 +1,23 @@
 <template>
   <p>Registration form here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  methods: {
+    register() {
+      //Assuming sucessful API call to register them
+
+      //Set a flash message to appear on the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
